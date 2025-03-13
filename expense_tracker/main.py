aa8ce0eac1243ed1eebfa99326eaa5a46f9e8955
Zyer459 <asiful.sumit@gmail.com>
2025-03-14 01:12:33 +0600

    update code for data visualization

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -1,5 +1,5 @@
 from database import add_expense, get_expense
-from reports import get_total_spent, get_spending_by_category
+from reports import *
 
 def main():
     while True:
@@ -7,11 +7,12 @@ def main():
         print("1. Add Expense")
         print("2. View Expenses")
         print("3. View Reports")
-        print("4. Exit")
+        print("4. Visualize data")
+        print("5. Exit")
 
-        choice = input("Select an option: ")
+        choice = int(input("Select an option: "))
 
-        if choice == "1":
+        if choice == 1:
             date = input("Enter date (YYYY-MM-DD): ")
             category = input("Enter category (Food, Transport, etc.): ")
             amount = float(input("Enter amount: "))
@@ -19,13 +20,13 @@ def main():
             add_expense(date, category, amount, description)
             print("Expense added successfully!")
 
-        elif choice == "2":
+        elif choice == 2:
             expenses = get_expense()
             print("\nExpenses:")
             for exp in expenses:
                 print(exp)
 
-        elif choice == "3":
+        elif choice == 3:
             total = get_total_spent()
             categories = get_spending_by_category()
             print(f"\nTotal Spent: ${total}")
@@ -33,7 +34,19 @@ def main():
             for category, amount in categories:
                 print(f"{category}: ${amount}")
 
-        elif choice == "4":
+        elif choice == 4:
+        	print('\n1. pie-chart: spending by category')
+        	print('2. bar-chart: spending trend over time')
+        	viz_choice = int(input('select an option: '))
+        	
+        	if viz_choice == 1:
+        		plot_spending_by_category()
+        	elif viz_choice == 2:
+        		plot_spending_trend()
+        	else:
+        		print('invalid choice')        
+        
+        elif choice == 5:
             print("Goodbye!")
             break
 
--- expense_tracker/reports.py
+++ expense_tracker/reports.py
@@ -1,4 +1,48 @@
 import sqlite3
+import matplotlib.pyplot as plt
+
+def plot_spending_by_category():
+	conn = sqlite3.connect("expense_tracker.db")
+	cursor = conn.cursor()
+	cursor.execute('SELECT category, SUM(amount) FROM expenses GROUP BY category')
+	data = cursor.fetchall()
+	conn.close
+	
+	if not data:
+		print('no data available for visualization')
+		return
+	
+	categories = [row[0] for row in data]
+	amounts = [row[1] for row in data]
+	
+	plt.figure(figsize=(7,7))
+	plt.pie(amounts, labels = categories, startangle = 90)
+	plt.title('Spending by category')
+	plt.show()
+	
+def plot_spending_trend():
+	conn = sqlite3.connect('expense_tracker.db')
+	cursor = conn.cursor()
+	cursor.execute('SELECT date, SUM(amount) FROM expenses GROUP BY date ORDER BY date')
+	data = cursor.fetchall()
+	conn.close()
+	
+	if not data:
+		print('no data for visualization')
+		return
+	
+	dates = [row[0] for row in data]
+	amounts = [row[1] for row in data]
+	
+	plt.figure(figsize = (10,5))
+	plt.bar(dates, amounts, color="#4c72b0", width = 0.5)
+	plt.xlabel('Date')
+	plt.ylabel('Total spent (tk)')
+	plt.xticks(rotation=45)
+	plt.title('Spending trend over time')
+	plt.grid(axis="y", linestyle="--", alpha=0.7)
+	plt.show()
+	
 
 def get_total_spent():
     conn = sqlite3.connect("expense_tracker.db")
